452f14b1c2cd216f0402dcd60b49c175961ccc05
邢志伟 <xingzhiwei8924@yundasys.com>
2021-02-24 09:37:15 +0800

    lambda表达式

--- src/main/java/cn/org/wyxxt/strategy/Comparator.java
+++ src/main/java/cn/org/wyxxt/strategy/Comparator.java
@@ -6,6 +6,13 @@ package cn.org.wyxxt.strategy;
  * @time 2021/2/23 6:17 下午
  * @email jsjxzw@163.com
  */
+
+@FunctionalInterface
 public interface Comparator<T> {
     int compare(T o1, T o2);
+
+    //1.8以后可以写实现方法
+    default void m() {
+        System.out.println("m");
+    }
 }
